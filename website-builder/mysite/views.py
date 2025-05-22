32e3f17099a92a0ff09137d2cf5149994b7fbc88
Joel Thomas <joel@codewithjoe.in>
2025-05-22 11:08:34 +0530

    feat : added caching in the website builder service for giving the website faster

--- website-builder/mysite/views.py
+++ website-builder/mysite/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from app.models import *
 from .serializers import *
 from .scope_decorator import user_permission
+from django.core.cache import cache
+from . utils import tenant_key_prefix , website_list_cache_key , website_cache_key , default_website_cache_key
 
 from . import constants
 
@@ -12,8 +14,14 @@ class WebsiteApiView(APIView):
 
     @user_permission(constants.HAS_BUILDER_PERMISSION)
     def get(self, request):
+        cache_key = website_list_cache_key(request.tenant)
+        data = cache.get(cache_key)
+        if data is not None:
+            print("From Cache")
+            return Response(data)
         queryset = Website.objects.filter(tenant=request.tenant).order_by("-is_default", "-updated_at")
         serializer = WebsiteSerialzer(queryset, many=True, context={'request': request})
+        cache.set(cache_key, serializer.data, timeout=None)
         return Response(serializer.data)
 
     @user_permission(constants.HAS_BUILDER_PERMISSION)    
@@ -22,6 +30,8 @@ class WebsiteApiView(APIView):
         serializer = WebsiteSerialzer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save(tenant=request.tenant,is_default=(not Website.objects.filter(tenant=request.tenant, is_default=True).exists()))
+            cache.delete(website_list_cache_key(request.tenant))
+            cache.delete(default_website_cache_key(request.tenant))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -33,6 +43,11 @@ class WebsiteApiView(APIView):
         serializer = WebsiteSerialzer(website, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            cache.delete(website_list_cache_key(request.tenant))
+            if website.is_default:
+                cache.delete(default_website_cache_key(request.tenant))
+            if 'id' in locals():  # for put and delete
+                cache.delete(website_cache_key(request.tenant, id))
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     @user_permission(constants.HAS_BUILDER_PERMISSION)   
@@ -41,6 +56,11 @@ class WebsiteApiView(APIView):
         if not website:
             return Response({'data': 'Website not found'}, status=status.HTTP_404_NOT_FOUND)
         website.delete()
+        cache.delete(website_list_cache_key(request.tenant))
+        if website.is_default:
+            cache.delete(default_website_cache_key(request.tenant))
+        if 'id' in locals():  # for put and delete
+            cache.delete(website_cache_key(request.tenant, id))
         return Response({'data': 'Website deleted successfully'}, status=status.HTTP_200_OK)
 
 
@@ -51,26 +71,37 @@ class GetWebsiteView(APIView):
     permission_classes = []
 
     def get(self, request, id):
+        cache_key = website_cache_key(request.tenant, id)
+        data = cache.get(cache_key)
+        if data:
+            print("From Cache")
+            return Response(data)
         website = Website.objects.filter(tenant=request.tenant, id=id).first()
         if not website:
             return Response({'data': 'Website not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = WebsiteSerialzer(website, context={'request': request})
+        cache.set(cache_key, serializer.data, timeout=None)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-
 
 class GetTenantDefaultWebpage(APIView):
     authentication_classes = []
     permission_classes = []
 
-
     def get(self, request):
-        website = Website.objects.filter(tenant=request.tenant , is_default=True)
-        # print(website.first().web_data)
-        if not website.exists():
-            return Response({'data' : "Website not found"} , status=status.HTTP_404_NOT_FOUND)
-        serializer = WebsiteSerialzer(website.first() , context = {'request' , request})
-        return Response(serializer.data , status=status.HTTP_200_OK)
+        cache_key = default_website_cache_key(request.tenant)
+        data = cache.get(cache_key)
+        if data:
+            print("From Cache")
+            return Response(data)
+
+        website = Website.objects.filter(tenant=request.tenant, is_default=True).first()
+        if not website:
+            return Response({'data': "Website not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = WebsiteSerialzer(website, context={'request': request})
+        cache.set(cache_key, serializer.data, timeout=60 * 10)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
@@ -89,6 +120,9 @@ class ChangeTenantDefaultWebpage(APIView):
         if not website.is_default:
             website.is_default = True
             website.save()
+            cache.delete(website_list_cache_key(request.tenant))
+            cache.delete(default_website_cache_key(request.tenant))
+            cache.delete(website_cache_key(request.tenant, id))
 
         serializer = WebsiteSerialzer(website, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
