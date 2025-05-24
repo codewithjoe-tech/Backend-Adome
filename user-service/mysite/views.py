8793f0a2500ac2150c428fdc75f506eb40b967d5
Joel Thomas <joel@codewithjoe.in>
2025-05-24 18:15:12 +0530

    fix : removed validation for old access token in refresh

--- user-service/mysite/views.py
+++ user-service/mysite/views.py
@@ -244,7 +244,7 @@ class RefreshTokenView(APIView):
         old_access_token = request.COOKIES.get(f'{request.tenant.subdomain}_access_token')
         # print(token , old_access_token)
 
-        if not (token and old_access_token):
+        if not (token):
             print(token , old_access_token)
             return Response({'error': 'Tokens missing'}, status=status.HTTP_401_UNAUTHORIZED)
 
